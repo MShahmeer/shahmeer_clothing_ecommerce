16d19b0b14750847dbc10933e3e46c8a3a2da02c
Muhammad Shahmeer <muhammadshahmeeronline@gmail.com>
2022-02-09 22:25:47 +0500

    separate component made and styling is added with images on the menu items

---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Directory from "../../Components/Directory/Directory"
+import './HomePageStyles.scss'
+const HomePage = () => {
+  return (
+    <div className="homepage">
+     <Directory/>
+    </div>
+  );
+};
+
+export default HomePage;
