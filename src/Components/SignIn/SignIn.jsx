be85cc6585d89833ba6e1203a073a6d6c350d4b7
Muhammad Shahmeer <muhammadshahmeeronline@gmail.com>
2022-02-12 03:46:12 +0500

    sign in with email is done

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import FormInput from "../FormInput/FormInput";
 import CustomButton from "../CustomButton/CustomButton";
-import { signInWithGoogle } from "../../Firebase/FirebaseUtils";
+import { auth,signInWithGoogle } from "../../Firebase/FirebaseUtils";
 
 import "./SignInStyles.scss";
 class SignIn extends Component {
@@ -14,10 +14,18 @@ class SignIn extends Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
 
-    this.setState({ email: "", password: "" });
+    const{email, password} = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password)
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
+
   };
 
   handleChange = (event) => {
